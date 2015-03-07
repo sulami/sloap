e038d4d387bf6c7288cfeb18f3b451aa5e69d13f
Robin Schroer <sulami@peerwire.org>
2015-03-07 23:47:59 +0100

    add optarg stuff

--- src/sloap.c
+++ src/sloap.c
@@ -1,14 +1,33 @@
+#include <getopt.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 
 #include "term.h"
 
 int
-main()
+main(int argc, char *argv[])
 {
-	double threshold = 10;
+	int opt;
 	time_t start, now;
+	double threshold = 900; /* 15 minutes by default. */
+
+	while ((opt = getopt(argc, argv, "ht:")) != -1) {
+		switch (opt) {
+		case 'h':
+			puts("Usage: %s [-t seconds] command");
+			puts("Use 'q' to abort timer");
+			return EXIT_SUCCESS;
+		case 't':
+			threshold = strtod(optarg, NULL);
+			break;
+		default:
+			fprintf(stderr, "Usage: %s [-t seconds] command\n",
+			        argv[0]);
+			return EXIT_FAILURE;
+		}
+	}
 
 	puts("SLOAP started.");
 
@@ -22,7 +41,7 @@ main()
 				/* 'q', not going to include keysyms here. */
 				puts("SLOAP exited.\r");
 
-				return 0;
+				return EXIT_SUCCESS;
 			}
 
 			puts("Timer has been reset.\r");
@@ -34,11 +53,11 @@ main()
 		if (difftime(now, start) > threshold)
 			break;
 
-		sleep(1); /* be nice to the cpu */
+		sleep(1); /* Be nice to the CPU. */
 	}
 
 	puts("Wakeup time!\r");
 
-	return 0;
+	return EXIT_SUCCESS;
 }
 
